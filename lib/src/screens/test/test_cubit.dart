c9f734c52fbe3ed4553d0458d46c16ad7d3ec1ef
taloengrat.p <arm.taloengrat.superdev@gmail.com>
2024-02-10 13:57:11 +0700

    remove file

--- lib/src/screens/test/test_cubit.dart
+++
@@ -1,6 +0,0 @@
-import 'package:wolzarm_flutter_cli/src/screens/test/test_state.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-class TestCubit extends Cubit<TestState> {
-  TestCubit() : super(TestInitial()) {}
-}
